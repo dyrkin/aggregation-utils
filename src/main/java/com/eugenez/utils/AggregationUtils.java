638b808233a2d8a16f78e0591d712e4f78deb390
Eugene Zadyra <untrance@gmail.com>
2013-04-01 16:17:58 +0300

    A little bit refactored code

--- src/main/java/com/eugenez/utils/AggregationUtils.java
+++ src/main/java/com/eugenez/utils/AggregationUtils.java
@@ -55,13 +55,14 @@ public class AggregationUtils {
         return null;
     }
 
+    public static <Z> Collection<Z> list(Collection<?> collection, Z dummy) throws AggregationException {
+        return null;
+    }
+
     private static <Z> Z aggregate(Collection<?> collection, Aggregator<Z> aggregator, MethodEntry methodEntry) throws AggregationException {
         for (Object element : collection) {
             try {
-                Z result = (Z) hierarchyCall(element, methodEntry, aggregator);
-                if (result != null) {
-                    aggregator.add(result);
-                }
+                hierarchyCall(element, methodEntry, aggregator);
             } catch (IllegalAccessException e) {
                 throw new AggregationException("Error occurred", e);
             } catch (InvocationTargetException e) {
@@ -71,27 +72,44 @@ public class AggregationUtils {
         return aggregator.getResult();
     }
 
-    private static Object hierarchyCall(Object element, MethodEntry methodEntry, Aggregator aggregator) throws IllegalAccessException, InvocationTargetException {
-        if (element instanceof Iterable && methodEntry.getMethod().getName().equals("get")
-                && methodEntry.getArgs() != null && methodEntry.getArgs().length == 1
-                && (Integer) methodEntry.getArgs()[0] == -1) {
-            Iterator iterator = ((Iterable) element).iterator();
-            int i = 0;
-            while (iterator.hasNext()) {
-                MethodEntry entry = new MethodEntry(methodEntry.getMethod(), new Object[]{i});
-                entry.setReturnType(methodEntry.getReturnType());
-                entry.setNextMethodEntry(methodEntry.getNextMethodEntry());
-                aggregator.add(hierarchyCall(element, entry, aggregator));
-                iterator.next();
-                i++;
-            }
-            return null;
+    private static void hierarchyCall(Object element, MethodEntry methodEntry, Aggregator aggregator) throws IllegalAccessException, InvocationTargetException {
+        if (isCollectionRequiresIteration(element, methodEntry)) {
+            iterateCollection(element, methodEntry, aggregator);
         } else {
             Object potentialResult = methodEntry.getMethod().invoke(element, methodEntry.getArgs());
             if (methodEntry.getNextMethodEntry() != null) {
-                return hierarchyCall(potentialResult, methodEntry.getNextMethodEntry(), aggregator);
+                hierarchyCall(potentialResult, methodEntry.getNextMethodEntry(), aggregator);
+            } else {
+                aggregator.add(potentialResult);
             }
-            return potentialResult;
+        }
+    }
+
+    /**
+     * If get method of collection has index argument equals -1 then this means the
+     * we should do hierarchical call for every element in this collection
+     *
+     * @param element - any object
+     * @param methodEntry
+     * @return true in case if element is collection, method called equals to "get",
+     * method "get" has one argument and it's value = -1
+     */
+    private static boolean isCollectionRequiresIteration(Object element, MethodEntry methodEntry) {
+        return element instanceof Iterable && methodEntry.getMethod().getName().equals("get")
+                && methodEntry.getArgs() != null && methodEntry.getArgs().length == 1
+                && (Integer) methodEntry.getArgs()[0] == -1;
+    }
+
+    private static void iterateCollection(Object element, MethodEntry methodEntry, Aggregator aggregator) throws IllegalAccessException, InvocationTargetException {
+        Iterator iterator = ((Iterable) element).iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            MethodEntry entry = new MethodEntry(methodEntry.getMethod(), new Object[]{i});
+            entry.setReturnType(methodEntry.getReturnType());
+            entry.setNextMethodEntry(methodEntry.getNextMethodEntry());
+            hierarchyCall(element, entry, aggregator);
+            iterator.next();
+            i++;
         }
     }
 
@@ -106,12 +124,12 @@ public class AggregationUtils {
         MethodEntry methodEntry = MethodMagic.invokedMethodHierarchy.get();
         MethodMagic.invokedMethodHierarchy.set(null);
 
-        return getBootomEntry(methodEntry);
+        return getEntryFromTheBottomOfHierarchy(methodEntry);
     }
 
-    private static MethodEntry getBootomEntry(MethodEntry methodEntry) {
+    private static MethodEntry getEntryFromTheBottomOfHierarchy(MethodEntry methodEntry) {
         if (methodEntry.getPreviousMethod() != null) {
-            return getBootomEntry(methodEntry.getPreviousMethod());
+            return getEntryFromTheBottomOfHierarchy(methodEntry.getPreviousMethod());
         }
         return methodEntry;
     }
