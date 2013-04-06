2653094703e9368474215e35bf1e1925f9b6c412
Eugene Zadyra <untrance@gmail.com>
2013-04-06 22:23:17 +0300

    Done with Enhancer tests.

--- src/test/java/org/eugenez/utils/EnhancerTest.java
+++ src/test/java/org/eugenez/utils/EnhancerTest.java
@@ -59,6 +59,74 @@ public class EnhancerTest {
         assertNull(secondLevelMethodEntry.getNextMethodEntry());
     }
 
+
+    @Test
+    public void testTwoLevelsCollectionSizeCall() {
+        e(Building.class).getApartments().size();
+        MethodEntry methodEntry = getEntryFromTheBottomOfHierarchy(Enhancer.invokedMethodHierarchy.get());
+        assertNotNull(methodEntry);
+        assertNotNull(methodEntry.getMethod());
+        assertEquals("getApartments", methodEntry.getMethod().getName());
+        assertEquals(0, methodEntry.getArgs().length);
+        assertEquals(List.class, methodEntry.getReturnType());
+        assertNotNull(methodEntry.getNextMethodEntry());
+        MethodEntry secondLevelMethodEntry = methodEntry.getNextMethodEntry();
+        assertNotNull(secondLevelMethodEntry);
+        assertNotNull(secondLevelMethodEntry.getMethod());
+        assertEquals("size", secondLevelMethodEntry.getMethod().getName());
+        assertEquals(0, secondLevelMethodEntry.getArgs().length);
+        assertEquals(Integer.TYPE, secondLevelMethodEntry.getReturnType());
+        assertNull(secondLevelMethodEntry.getNextMethodEntry());
+    }
+
+    @Test
+    public void testTwoLevelsCollectionGetCall() {
+        e(Building.class).getApartments().get(0);
+        MethodEntry methodEntry = getEntryFromTheBottomOfHierarchy(Enhancer.invokedMethodHierarchy.get());
+        assertNotNull(methodEntry.getNextMethodEntry());
+        MethodEntry secondLevelMethodEntry = methodEntry.getNextMethodEntry();
+        assertNotNull(secondLevelMethodEntry);
+        assertNotNull(secondLevelMethodEntry.getMethod());
+        assertEquals("get", secondLevelMethodEntry.getMethod().getName());
+        assertEquals(1, secondLevelMethodEntry.getArgs().length);
+        assertEquals(0, secondLevelMethodEntry.getArgs()[0]);
+        assertEquals(Apartment.class, secondLevelMethodEntry.getReturnType());
+        assertNull(secondLevelMethodEntry.getNextMethodEntry());
+    }
+
+
+    @Test
+    public void testMultiLevelsCollectionCall() {
+        e(Building.class).getApartments().get(0).getResidents().get(-1).getAge();
+        MethodEntry methodEntry = getEntryFromTheBottomOfHierarchy(Enhancer.invokedMethodHierarchy.get());
+        assertNotNull(methodEntry.getNextMethodEntry());
+        assertNotNull(methodEntry.getNextMethodEntry().getNextMethodEntry());
+        MethodEntry thirdLevelMethodEntry = methodEntry.getNextMethodEntry().getNextMethodEntry();
+        assertNotNull(thirdLevelMethodEntry);
+        assertNotNull(thirdLevelMethodEntry.getMethod());
+        assertEquals("getResidents", thirdLevelMethodEntry.getMethod().getName());
+        assertEquals(0, thirdLevelMethodEntry.getArgs().length);
+        assertEquals(List.class, thirdLevelMethodEntry.getReturnType());
+
+        assertNotNull(thirdLevelMethodEntry.getNextMethodEntry());
+        MethodEntry fourthLevelMethodEntry = thirdLevelMethodEntry.getNextMethodEntry();
+        assertNotNull(fourthLevelMethodEntry);
+        assertNotNull(fourthLevelMethodEntry.getMethod());
+        assertEquals("get", fourthLevelMethodEntry.getMethod().getName());
+        assertEquals(1, fourthLevelMethodEntry.getArgs().length);
+        assertEquals(-1, fourthLevelMethodEntry.getArgs()[0]);
+        assertEquals(Resident.class, fourthLevelMethodEntry.getReturnType());
+
+        assertNotNull(fourthLevelMethodEntry.getNextMethodEntry());
+        MethodEntry fifthLevelMethodEntry = fourthLevelMethodEntry.getNextMethodEntry();
+        assertNotNull(fifthLevelMethodEntry);
+        assertNotNull(fifthLevelMethodEntry.getMethod());
+        assertEquals("getAge", fifthLevelMethodEntry.getMethod().getName());
+        assertEquals(0, fifthLevelMethodEntry.getArgs().length);
+        assertEquals(Integer.TYPE, fifthLevelMethodEntry.getReturnType());
+        assertNull(fifthLevelMethodEntry.getNextMethodEntry());
+    }
+
     private static MethodEntry getEntryFromTheBottomOfHierarchy(MethodEntry methodEntry) {
         if (methodEntry.getPreviousMethod() != null) {
             return getEntryFromTheBottomOfHierarchy(methodEntry.getPreviousMethod());
